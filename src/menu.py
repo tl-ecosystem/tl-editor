206b418d7e494ada09a1a05b22330bc451f49721
Theooking <nikosav2007@gmail.com>
2024-08-03 22:06:02 +0300

    New Event manager works

--- src/menu.py
+++ src/menu.py
@@ -1,34 +1,37 @@
-# from dataclasses import dataclass
-
+import pygame
 from pygame import FRect
+
+from dataclasses import dataclass
 
 from .tleng2 import *
 
-from .tleng2.ecs.events import EventComp, HandleEventsSystem, pygame_quit_handler
+# from .tleng2.ecs.events import EventComp, HandleEventsSystem, pygame_quit_handler
 from .tleng2.object.area import AreaComp
-from .tleng2.components.camera import *
+from .tleng2.components.camera import MainCameraComp, CameraComp
 from .tleng2.components.renderable import DisplayCanvasComp, RenderableComp
 from .tleng2.systems.engine_syst import ClockTickSystem
-from .tleng2.systems.renderer import RendererSystem
+from .tleng2.systems.renderer import RendererSystem, ResizeWindowEvent
 from .tleng2.uix.ui_canvas import UICanvas
 # from .tleng2.uix import UICanvas, UICanvasDrawSystem, BoxLayout
 from .tleng2.utils.event_manager import set_handler
 from .tleng2.utils.colors import AQUAMARINE, WHITESMOKE
 
 
-from typing import Callable as _Callable
-# from typing import   
-
-
-world = ecs.World()
+world = ecs.World(events=True)
 
 RendererProperties.fill_screen_color = AQUAMARINE
+
+@dataclass
+class QuitGameEvent: ...
 
 world.append_unique_components(
     {
         DisplayCanvasComp: DisplayCanvasComp(GlobalSettings._win_res),
-        EventComp : EventComp(
-            (pygame.QUIT, 'QUIT'),
+        ecs.EventsComp : ecs.EventsComp(
+            [
+                QuitGameEvent,
+                ResizeWindowEvent,
+            ]
         )
     }
 )
@@ -36,6 +39,7 @@ world.append_unique_components(
 display_canvas = world.spawn(
     DisplayCanvasComp(GlobalSettings._win_res)
 )
+
 
 # Game Enviroment Camera
 camera1 = world.spawn(
@@ -92,6 +96,23 @@ ui = world.spawn(
 
 world_scheduler = ecs.Schedule()
 
+
+class HandleEventsSystem(ecs.System):
+    def update(self) -> None:
+        for event in EngineProperties._events:
+            if event.type == pygame.QUIT:
+                self.world.events.send(QuitGameEvent())
+            if event.type == pygame.WINDOWRESIZED:
+                self.world.events.send(ResizeWindowEvent())
+
+
+class QuitGameSystem(ecs.System):
+    def update(self) -> None:
+        events = self.world.events.read(QuitGameEvent)
+        if events:
+            EngineProperties.GAME_RUNNING = False
+
+
 class LogicSystem(ecs.System):
     def update(self) -> None:
         # print(self.world.schedule.system_schedule[1]._display)
@@ -102,12 +123,14 @@ class LogicSystem(ecs.System):
 
 
 
-set_handler('QUIT', pygame_quit_handler)
+# set_handler('QUIT', pygame_quit_handler)
 # set_handler('RESIZE', )
 
 world_scheduler.add_systems(
+    ecs.EventManagerSystem(11),
     HandleEventsSystem(10),
     LogicSystem(1),
+    QuitGameSystem(),
     # DrawUICanvasSystem(0),
     RendererSystem(-1),
     ClockTickSystem(-2)
